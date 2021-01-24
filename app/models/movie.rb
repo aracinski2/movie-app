b55726d4df360940c075d775e0bed3a20918d1e2
Alex Racinski <aracinski2@gmail.com>
2021-01-24 11:26:49 -0600

    add validations to movie model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,6 @@
 class Movie < ApplicationRecord
+  validates :title, presence: true
+  validates :year, presence: true
+  validates :year, numericality: true
+  validates :plot, length: {minimum: 30}
 end

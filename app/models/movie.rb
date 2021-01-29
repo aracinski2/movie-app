073bc8eb59baba6a84d2a09aefd64bfdc78b4621
Alex Racinski <aracinski2@gmail.com>
2021-01-29 17:01:55 -0600

    add associations to for movie_genres

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -4,4 +4,6 @@ class Movie < ApplicationRecord
   validates :year, numericality: true
   validates :plot, length: {minimum: 30}
   has_many :actors
+  has_many :movie_genres
+  has_many :genres, through: :movie_genres
 end
